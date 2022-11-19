ad35f80895fa3a14c2d40b62e55fe8f0d02fa16e
Jesús Millán <jgmc3012@gmail.com>
2022-11-19 18:22:55 -0400

    :recycle: Merge ListFilterModelMixin with FilterBackend

--- rendalo/mixins.py
+++ rendalo/mixins.py
@@ -1,11 +1,20 @@
+from django.db.models import QuerySet
 from rest_framework.mixins import ListModelMixin
-from rendalo.backends import FilterBackend
 from rendalo.filters import Filter
 
 
 class ListFilterModelMixin(ListModelMixin):
-    filter_backends = (FilterBackend,)
 
     @property
     def filter_class(self)->Filter:
         raise NotImplementedError
+
+    def filter_queryset(self, queryset: QuerySet)-> QuerySet:
+        filter = self.filter_class(queryset)
+        for key, value in self.request.query_params.items():
+            function = getattr(filter, f"by_{key}", None)
+            if not callable(function):
+                continue
+            function(value)
+
+        return filter.queryset
